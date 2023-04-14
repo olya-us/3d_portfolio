1b7e670de9f989bd7ca8bd4e94d243ddbcaae6bf
Olya <o.us@sw-tech.by>
2023-04-14 15:14:59 +0300

    Create Works component

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,7 @@ import logo from "./logo-color.svg";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
-import github from "./github.png";
+import link from "./link.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
@@ -26,7 +26,7 @@ export {
   creator,
   mobile,
   web,
-  github,
+  link,
   menu,
   close,
   css,
